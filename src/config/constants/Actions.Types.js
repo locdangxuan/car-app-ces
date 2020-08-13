cd943277ccc20001132d0334168947f0e2c39e2b
unknown <Phatnguyen27>
2020-08-13 12:00:08 +0700

    remove unused constants

--- src/config/constants/Actions.Types.js
+++
@@ -1,5 +0,0 @@
-export const MODAL_ON = 'SHOW_THE_MODAL';
-export const MODAL_OFF = 'HIDE_THE_MODAL';
-export const REGISTER = 'REGISTER';
-export const LOGIN = 'LOGIN';
-export const UPDATE_FIELD_AUTH = 'UPDATE_FIELD_AUTH';
--- src/config/constants/Auth.ActionTypes.js
+++
@@ -1,3 +0,0 @@
-export const REGISTER = 'REGISTER';
-export const LOGIN = 'LOGIN';
-export const UPDATE_FIELD_AUTH = 'UPDATE_FIELD_AUTH';
