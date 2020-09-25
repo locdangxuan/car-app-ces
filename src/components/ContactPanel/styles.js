18818ed8570f27eb5bf6ca7345598b8e8ab98cd2
Hậu Nguyễn Trần <nguyentranhau98@gmail.com>
2020-09-25 08:26:00 +0000

    created contact page

---
+++ src/components/ContactPanel/styles.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import Color from 'config/constants/Colors';
+import { Span } from 'components/common';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+    Box: {
+        display: 'flex',
+        flexDirection: 'row',
+    },
+}));
+
+const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 5vh;
+`;
+
+const Body = styled.div`
+    background-color: ${Color.backgroundDetails};
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    text-align: justify;
+`;
+
+const StyledSpan = styled(Span)`
+    font-size: 20px;
+    margin: 0 10vh;
+`;
+
+const theme = {
+    span: {
+        color: Color.white,
+    },
+};
+
+export { Wrapper, Body, StyledSpan, theme, useStyles };
